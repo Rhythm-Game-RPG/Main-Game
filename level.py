eb70931a89bd507edcb02e1b84ce7f9b9df32221
Kaleb Szaro <kaleb.szaro@gmail.com>
2024-04-28 18:20:52 -0400

    New Title Screen and Set up for Levels

--- level.py
+++ level.py
@@ -24,7 +24,7 @@ class Level:
         self.pause = Pause(self.player)
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
+        for row_index, row in enumerate(LEVEL1):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
@@ -32,7 +32,7 @@ class Level:
                     Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
                 if col == 'p':
                     self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites, self.monster_list)
-        for row_index, row in enumerate(WORLD_MAP):
+        for row_index, row in enumerate(LEVEL1):
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
