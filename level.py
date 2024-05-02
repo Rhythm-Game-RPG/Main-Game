b6b9fa5149a9a6c3a5da9d33e4a0fe8582f76a73
Kaleb Szaro <kaleb.szaro@gmail.com>
2024-05-01 23:32:18 -0400

    Added Ghost to levels

--- level.py
+++ level.py
@@ -13,6 +13,7 @@ from skeleton import Skeleton
 from bat import Bat
 from minotaur import Minotaur
 from chest import Chest
+from ghost import Ghost
 
 
 class Level:
@@ -85,6 +86,9 @@ class Level:
                     self.player.monster_list.append(self.monster)
                 if col == 'b':
                     self.monster = (Bat((x, y), [self.visible_sprites], self.obstacles_sprites, self.player))
+                    self.player.monster_list.append(self.monster)
+                if col == 'g':
+                    self.monster = (Ghost((x, y), [self.visible_sprites], self.obstacles_sprites, self.player))
                     self.player.monster_list.append(self.monster)
 
     def toggle_menu(self):
